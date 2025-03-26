4692ca05622ac33a74af79efcd3a844d3980ab99
Leticia <leticiamcdmedeiros@gmail.com>
2025-03-26 16:18:16 -0300

    Cadastro de produtos (view) - [Task 3.1]

--- src/main/java/edu/uepb/cct/cc/Main.java
+++ src/main/java/edu/uepb/cct/cc/Main.java
@@ -2,6 +2,7 @@ package edu.uepb.cct.cc;
 
 import edu.uepb.cct.cc.view.CompradorView;
 import edu.uepb.cct.cc.view.LojaView;
+import edu.uepb.cct.cc.view.ProdutoView;
 
 import java.util.Scanner;
 
@@ -27,7 +28,9 @@ public class Main {
                 case 2 -> compradorView.listarCompradores();
                 case 3 -> compradorView.atualizarComprador();
                 case 4 -> compradorView.deletarComprador();
-                case 0 -> { return; }
+                case 0 -> {
+                    return;
+                }
                 default -> System.out.println("Opção inválida. Tente novamente.");
             }
         }
@@ -50,7 +53,36 @@ public class Main {
             switch (escolha) {
                 case 1 -> lojaView.cadastrarLoja();
                 case 2 -> lojaView.deletarLoja();
-                case 0 -> { return; }
+                case 0 -> {
+                    return;
+                }
+                default -> System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
+
+    public static void menuProduto() {
+        ProdutoView produtoView = new ProdutoView();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n=== Menu Produto ===");
+            System.out.println("1. Cadastrar Produto");
+            System.out.println("2. Listar produtos");
+            System.out.println("3. Atualizar Produto");
+            System.out.println("4. Deletar produto");
+            System.out.println("0. Voltar");
+            System.out.print("Escolha uma opção: ");
+
+            int escolha = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (escolha) {
+                case 1 -> produtoView.cadastrarProduto();
+                case 2 -> System.out.println("2");
+                case 0 -> {
+                    return;
+                }
                 default -> System.out.println("Opção inválida. Tente novamente.");
             }
         }
@@ -63,6 +95,7 @@ public class Main {
             System.out.println("\n=== Menu Principal ===");
             System.out.println("1. Menu Comprador");
             System.out.println("2. Menu Loja");
+            System.out.println("3. Menu Produto");
             System.out.println("0. Sair");
             System.out.print("Escolha uma opção: ");
 
@@ -72,6 +105,7 @@ public class Main {
             switch (escolha) {
                 case 1 -> menuComprador();
                 case 2 -> menuLoja();
+                case 3 -> menuProduto();
                 case 0 -> {
                     System.out.println("Saindo...");
                     return;
